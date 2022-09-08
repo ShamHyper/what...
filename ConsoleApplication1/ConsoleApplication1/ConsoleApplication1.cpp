e28a7da680ef822921f121f2a113eb0c07b651fe
Shatts Makar <104412752+ShamHyper@users.noreply.github.com>
2022-09-08 14:04:28 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+int main(void)
+{
+	system("chcp 1251");
+	int x, b1000, a;
+	printf("Введите число 1:");
+	scanf(R"(%d)", &x);
+	if (x == 1)
+	{
+		printf("Слава России\n");
+	}
+	else
+	{
+		printf("Ты еблан?\n");
+	}
+	_getch();
+	return 0;
+}
